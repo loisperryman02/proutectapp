6ed0522f6c595ad5820ce41cf079828990118df6
loisperryman02 <loisperryman02@gmail.com>
2024-02-05 15:36:40 +0000

    Added expo, data structures inside mobile-applicaiton file.

---
+++ mobile-application/data_structures/aStar.js
@@ -0,0 +1,185 @@
+
+// A* Graph Search algorithm. 
+
+// Need to sort out objects and how they are accessed in this file.
+export function aStar(startNode, goalNode, nodes) {
+    let openSet = new Set(); // starts with start node
+    let exploredSet = new Set(); // defines already explored nodes
+    let cameFrom = new Map(); // reconstructs path later
+    let gScore = {}; // Cost from start to node
+    let fScore = {}; // estimated cost from start to goal through node
+
+    console.log("STARTING A*");
+
+    openSet.add(startNode.id);
+
+    // Sets it to the highest possible value, so it can be updated later
+    gScore[startNode.id] = 0;
+    fScore[startNode.id] = 0;
+
+    goal = {
+        id: goalNode.id,
+        edges: goalNode.edges
+    };
+
+    while (openSet.size > 0) {
+
+        console.log("open set before");
+        console.log(openSet);
+
+        let currentId = null;
+
+        lowestFScore = Infinity;
+
+        // Loops through openSet, gets the node with the lowest fScore and sets it to current 
+        openSet.forEach(nodeId => {
+            if (fScore[nodeId] < lowestFScore) {
+                currentId = nodeId;
+                lowestFScore = fScore[nodeId];
+            }
+        });
+
+        // if no current node, break out of loop. 
+        if (currentId == null) break;
+
+        current = nodes.get(currentId);
+
+        openSet.delete(currentId);
+        exploredSet.add(currentId);
+
+        console.log("open set after");
+        console.log(openSet);
+
+
+        console.log(currentId);
+
+        console.log("edges...");
+        console.log(current.edges)
+
+        for (let edge of current.edges) {
+            let neighbourId = edge.to;
+            let neighbour = nodes.get(neighbourId);
+
+            console.log("neighbour");
+            console.log(neighbourId);
+
+            if (exploredSet.has(neighbourId)) {
+                continue;
+            }
+
+            if (neighbourId === goalNode.id) {
+                console.log("Found goal id!");
+                cameFrom.set(neighbourId, current);
+                console.log(cameFrom);
+                return reconstructPath(cameFrom, neighbour);
+            }
+
+            let temporaryCost = costFunction(edge);
+
+            // Calculates a temporary gScore for neighbour node 
+            // Sum of gScore of current node, and the cost of current node and neighbour (considering distance and safety score)
+            let temporaryGScore = gScore[current.id] + temporaryCost;
+
+            console.log("temp g score");
+            console.log(gScore[current.id]);
+            console.log(current);
+            console.log(gScore);
+            
+
+            let temporaryFScore = temporaryGScore + heuristicCostEstimate(neighbour, goal);
+
+            console.log(heuristicCostEstimate(neighbour, goal))
+
+            console.log("temp f score");
+            console.log(temporaryFScore);
+
+            // This checks if the tentative gScore for neighbour node is better than previously recorded
+            // If neighbour node hasn't been encountered before, infinity is assigned to gScore
+
+            if (!fScore[neighbourId] || temporaryFScore < fScore[neighbourId]) {
+
+                cameFrom.set(neighbour.id, current);
+                gScore[neighbourId] = temporaryGScore;
+                fScore[neighbourId] = temporaryFScore;
+
+                if (!openSet.has(neighbourId)) {
+                    openSet.add(neighbourId);
+                };
+            }
+            
+            console.log("came From");
+            console.log(cameFrom);
+        }
+    }
+
+    return [];
+}
+
+
+function haversineDistance(coords1, coords2, isMiles = false) {
+    
+    // Earth's radius in kilometers or miles
+    const RADIUS_OF_EARTH_IN_KM = 6371;
+    const RADIUS_OF_EARTH_IN_MILES = 3959;
+  
+    const radius = isMiles ? RADIUS_OF_EARTH_IN_MILES : RADIUS_OF_EARTH_IN_KM;
+
+    firstCoordinates = coords1.split(",");
+    const lat1 = parseFloat(firstCoordinates[0]);
+    const lon1 = parseFloat(firstCoordinates[1]);
+
+    secondCoordinates = coords2.split(",");
+    const lat2 = parseFloat(secondCoordinates[0]);
+    
+    const lon2 = parseFloat(secondCoordinates[1]);
+
+    const latDelta = deg2rad(lat2 - lat1);
+    const lonDelta = deg2rad(lon2 - lon1);
+  
+    const a = Math.sin(latDelta / 2) * Math.sin(latDelta / 2) +
+                Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+                Math.sin(lonDelta / 2) * Math.sin(lonDelta / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  
+    return radius * c;
+  }
+
+  
+// Helper function to convert degrees to radians
+function deg2rad(degrees) {
+    return degrees * (Math.PI / 180);
+}
+
+
+function heuristicCostEstimate(currentNode, goalNode) {
+    return haversineDistance(currentNode.id, goalNode.id);
+}
+
+function costFunction(edge) {
+    // The higher the safety score, the lower the cost of the edge. 
+    console.log(edge.distance);
+    console.log(edge.safetyScore);
+    return parseFloat(edge.distance) * parseFloat(1/edge.safetyScore * 100) ;
+}
+
+// Iterates backwards from the goal node to the start node using cameFrom map
+// cameFrom map -> keeps track of the best predessecor for each node
+// This function gets the entire sequence of steps taken once the goal is found. 
+function reconstructPath(cameFrom, goal) {
+    let goalId = goal.id;
+    let totalPath = [goalId];
+    console.log("total path");
+    console.log(totalPath);
+
+    while (cameFrom.has(goalId)) {
+        let predessecorNode = cameFrom.get(goalId);
+        console.log("predessecor node");
+        console.log(predessecorNode);
+        totalPath.unshift(predessecorNode.id);
+        goalId = predessecorNode.id;
+        console.log("currentID at end");
+        console.log(goalId);
+    }
+
+    return totalPath;
+}
